bee23636f73593e2576693abb03ec885be1e9000
Aditya Shiyale <adityashiyale7219@gmail.com>
2025-11-14 19:40:34 +0530

    Add Tailwind CSS and NativeWind configuration for styling suggest branch name

---
+++ client/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        Primary: "#6C63FF",
+        accent: "#FF6BAA",
+        text_primary: "#1E1E1E",
+        text_secondary: "#6E6E6E",
+        custom_border: "rgba(108, 99, 255, 0.1)",
+      },
+    },
+  },
+  plugins: [],
+};
